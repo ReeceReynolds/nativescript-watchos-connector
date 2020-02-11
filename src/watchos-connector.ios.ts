0bfabb049094552f8656c4d314bda1c79b4ccf0c
Reece Reynolds <r.reynolds@aitch-c.com>
2020-02-11 13:39:03 +0000

    Added methods to create WCSession and send data to the Watch

--- src/watchos-connector.ios.ts
+++ src/watchos-connector.ios.ts
@@ -1,5 +1,108 @@
-import { Common } from './watchos-connector.common';
+import { isIOS } from 'tns-core-modules/platform/platform'
 
-export class WatchosConnector extends Common {
+export class WatchOSConnector implements WCSessionDelegate {
+  watchSession: any
 
+  // Creates a WCSession
+  createWCSession() {
+    if (WCSession.isSupported) {
+      this.watchSession = WCSession.defaultSession
+      this.watchSession.delegate = this.watchSession
+      this.watchSession.activateSession()
+    }
+  }
+
+  // Use instead of 'isIOS' when working with WatchOS
+  // Checks if the user is on iOS, has an Apple Watch paired and the WatchOS app is installed
+  watchOSChecker() {
+    if (
+      isIOS &&
+      this.watchSession.paired &&
+      this.watchSession.watchAppInstalled
+    ) {
+      return true
+    } else {
+      console.log("[watchOSChecker] WatchOS isn't available")
+      return false
+    }
+  }
+
+  // Checks if the WCSession is active (if not, session will be activated)
+  checkActivation() {
+    if (this.watchSession.activationState != 2) {
+      this.watchSession.activateSession()
+    }
+  }
+
+  // Send the data to the companion WatchOS App
+  packageAndSend(object: {}, objectKey: string) {
+    this.checkActivation()
+
+    // Send the object to the WatchOS app
+    let dictionary = NSDictionary.dictionaryWithObjectForKey(object, objectKey)
+    this.watchSession.updateApplicationContextError(dictionary)
+  }
+
+  // Needed to Conform to WCSessionDelegate - DON'T DELETE!!
+  sessionActivationDidCompleteWithStateError(
+    session: WCSession,
+    activationState: WCSessionActivationState,
+    error: NSError
+  ): void {
+    console.log(
+      '(sessionActivationDidCompleteWithStateError) WCSession Activated'
+    )
+  }
+  sessionDidBecomeInactive(session: WCSession): void {
+    console.log('(sessionDidBecomeInactive) Session Became Inactive')
+  }
+  sessionDidDeactivate(session: WCSession): void {
+    console.log('(sessionDidDeactivate) Session Deactivated')
+  }
+  sessionReachabilityDidChange?(session: WCSession): void {}
+  sessionWatchStateDidChange?(session: WCSession): void {}
+
+  // Not Used but Needed for WCSessionDelegate - DON'T DELETE!!
+  debugDescription?: string
+  description: string
+  hash: number
+  isProxy: boolean
+  superclass: typeof NSObject
+  class(): typeof NSObject {
+    throw new Error('Method not implemented.')
+  }
+  conformsToProtocol(aProtocol: any): boolean {
+    throw new Error('Method not implemented.')
+  }
+  isEqual(object: any): boolean {
+    throw new Error('Method not implemented.')
+  }
+  isKindOfClass(aClass: typeof NSObject): boolean {
+    throw new Error('Method not implemented.')
+  }
+  isMemberOfClass(aClass: typeof NSObject): boolean {
+    throw new Error('Method not implemented.')
+  }
+  performSelector(aSelector: string) {
+    throw new Error('Method not implemented.')
+  }
+  performSelectorWithObject(aSelector: string, object: any) {
+    throw new Error('Method not implemented.')
+  }
+  performSelectorWithObjectWithObject(
+    aSelector: string,
+    object1: any,
+    object2: any
+  ) {
+    throw new Error('Method not implemented.')
+  }
+  respondsToSelector(aSelector: string): boolean {
+    throw new Error('Method not implemented.')
+  }
+  retainCount(): number {
+    throw new Error('Method not implemented.')
+  }
+  self(): NSObjectProtocol {
+    throw new Error('Method not implemented.')
+  }
 }
